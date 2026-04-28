8d6c12d2de77c809c647641d5470db0b6fcfd0a4
khangnguyen0810 <gravegun@gmail.com>
2026-04-28 19:41:33 +0700

    reconstruct project structure

--- server/ai-resume-builder/src/main/java/com/khangnguyen/ai_resume_builder/dto/UploadResponseDTO.java
+++ server/ai-resume-builder/src/main/java/com/khangnguyen/ai_resume_builder/dto/UploadResponseDTO.java
@@ -1,4 +1,12 @@
 package com.khangnguyen.ai_resume_builder.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class UploadResponseDTO {
+    private String fileId;
+    private String fileName;
+    private String message;
 }
